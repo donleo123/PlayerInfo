7bdb1479a2d21a70e1fb24a95a1007e3b9c52e2e
donleo123 <leonnijboer7@hotmail.com>
2017-04-06 17:32:53 +0200

    Add files via upload

--- src/me/donleo123/main/Main.java
+++ src/me/donleo123/main/Main.java
@@ -1,36 +1,113 @@
 package me.donleo123.main;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin implements Listener {
 
 	public void onEnable() {
 		getLogger().info("-----------------------");
-		getLogger().info(getName() + "is Enabled!");
+		getLogger().info(getName() + " is Enabled!");
 		getLogger().info("-----------------------");
 	}
-	
+
 	public void onDisable() {
 		getLogger().info("-----------------------");
-		getLogger().info(getName() + "is Disabled!");
+		getLogger().info(getName() + " is Disabled!");
 		getLogger().info("-----------------------");
+		Bukkit.getServer().getPluginManager().registerEvents(this, this);
 	}
-	
+
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		Player p = (Player)sender;
-		if(label.equalsIgnoreCase("playerinfo") && sender.hasPermission("playerinfo.info")) {
-            p.sendMessage(ChatColor.BLACK + "-=" + ChatColor.DARK_RED + " Player's Info " + ChatColor.BLACK + "=-");
-			p.sendMessage(ChatColor.YELLOW + "Player name: " + ChatColor.AQUA + p.getName());
-			p.sendMessage(ChatColor.YELLOW + "UUID: " + ChatColor.AQUA + p.getUniqueId());
-			p.sendMessage("");
-			p.sendMessage(ChatColor.BLACK + "-=" + ChatColor.DARK_RED + " Item Info " + ChatColor.BLACK + "=-");
-			p.sendMessage(ChatColor.YELLOW + "Current item in hand: " + ChatColor.AQUA + p.getItemInHand().getType());
-			p.sendMessage(ChatColor.YELLOW + "Amount: " + ChatColor.AQUA + p.getItemInHand().getAmount());
+		if (sender instanceof Player) {
+			Player p = (Player) sender;
+			if (label.equalsIgnoreCase("playerinfo") && sender.hasPermission("playerinfo.other")) {
+
+				if (args.length == 0) {
+					sender.sendMessage("§cIncorrect Player Name!");
+					return false;
+				} else {
+					Player pl = Bukkit.getPlayer(args[0]);
+
+					if (pl == null) {
+						sender.sendMessage("§cPlayer is not online.");
+
+						return false;
+					}
+
+					String uuid = p.getUniqueId().toString();
+					String language = me.smessie.MultiLanguage.bukkit.AdvancedMultiLanguageAPI.getLanguageOfUuid(uuid);
+
+					// ENGELS
+
+					if (language.equalsIgnoreCase("EN")) {
+						sender.sendMessage(ChatColor.BLACK + "-=" + ChatColor.DARK_RED + " Player's Info "+ ChatColor.BLACK + "=-");
+						sender.sendMessage(ChatColor.YELLOW + "Player's Name: " + ChatColor.AQUA + pl.getName());
+						sender.sendMessage(ChatColor.YELLOW + "UUID: " + ChatColor.AQUA + pl.getUniqueId());
+						sender.sendMessage("");
+						sender.sendMessage(ChatColor.BLACK + "-=" + ChatColor.DARK_RED + " Item Info " + ChatColor.BLACK + "=-");
+						sender.sendMessage(ChatColor.YELLOW + "Item in hand: " + ChatColor.AQUA + pl.getItemInHand().getType());
+						sender.sendMessage(ChatColor.YELLOW + "Amount: " + ChatColor.AQUA + pl.getItemInHand().getAmount());
+					}
+
+					// NEDERLANDS
+
+					if (language.equalsIgnoreCase("NL")) {
+						sender.sendMessage(ChatColor.BLACK + "-=" + ChatColor.DARK_RED + " Player's Info "
+								+ ChatColor.BLACK + "=-");
+						sender.sendMessage(ChatColor.YELLOW + "Speler naam: " + ChatColor.AQUA + pl.getName());
+						sender.sendMessage(ChatColor.YELLOW + "UUID: " + ChatColor.AQUA + pl.getUniqueId());
+						sender.sendMessage("");
+						sender.sendMessage(ChatColor.BLACK + "-=" + ChatColor.DARK_RED + " Item Info " + ChatColor.BLACK + "=-");
+						sender.sendMessage(ChatColor.YELLOW + "Item in hand: " + ChatColor.AQUA + pl.getItemInHand().getType());
+						sender.sendMessage(ChatColor.YELLOW + "Hoeveelheid: " + ChatColor.AQUA + pl.getItemInHand().getAmount());
+					}
+					// ENGELS
+				}
+
+			}
+
+		}
+
+		if (label.equalsIgnoreCase("playerinfo-me") && sender.hasPermission("playerinfo.me")) {
+
+			Player p = (Player) sender;
+			String uuid = p.getUniqueId().toString();
+			String language = me.smessie.MultiLanguage.bukkit.AdvancedMultiLanguageAPI.getLanguageOfUuid(uuid);
+
+			// NEDERLANDS
+
+			if (language.equalsIgnoreCase("NL")) {
+				sender.sendMessage(
+						ChatColor.BLACK + "-=" + ChatColor.DARK_RED + " Player's Info " + ChatColor.BLACK + "=-");
+				sender.sendMessage(ChatColor.YELLOW + "Speler naam: " + ChatColor.AQUA + p.getName());
+				sender.sendMessage(ChatColor.YELLOW + "UUID: " + ChatColor.AQUA + p.getUniqueId());
+				sender.sendMessage("");
+				sender.sendMessage(ChatColor.BLACK + "-=" + ChatColor.DARK_RED + " Item Info " + ChatColor.BLACK + "=-");
+				sender.sendMessage(ChatColor.YELLOW + "Item in hand: " + ChatColor.AQUA + p.getItemInHand().getType());
+				sender.sendMessage(ChatColor.YELLOW + "Hoeveelheid: " + ChatColor.AQUA + p.getItemInHand().getAmount());
+			}
+
+			// ENGELS
+
+			if (language.equalsIgnoreCase("EN")) {
+				sender.sendMessage(
+						ChatColor.BLACK + "-=" + ChatColor.DARK_RED + " Player's Info " + ChatColor.BLACK + "=-");
+				sender.sendMessage(ChatColor.YELLOW + "Your Name: " + ChatColor.AQUA + p.getName());
+				sender.sendMessage(ChatColor.YELLOW + "UUID: " + ChatColor.AQUA + p.getUniqueId());
+				sender.sendMessage("");
+				sender.sendMessage(ChatColor.BLACK + "-=" + ChatColor.DARK_RED + " Item Info " + ChatColor.BLACK + "=-");
+				sender.sendMessage(ChatColor.YELLOW + "Item in hand: " + ChatColor.AQUA + p.getItemInHand().getType());
+				sender.sendMessage(ChatColor.YELLOW + "Amount: " + ChatColor.AQUA + p.getItemInHand().getAmount());
+			}
+
 		}
 		return false;
 	}
+
 }
